ea923032d00271134e6ec73cd8ca49e41a367965
Titouan Vervack <Titouan.Vervack@UGent.be>
2013-03-23 16:11:02 +0100

    Fix

--- src/eu/pinnoo/appsforghent2013/view/GetWaitActivity.java
+++ src/eu/pinnoo/appsforghent2013/view/GetWaitActivity.java
@@ -3,7 +3,6 @@ package eu.pinnoo.appsforghent2013.view;
 import android.app.Activity;
 import android.os.Bundle;
 import eu.pinnoo.appsforghent2013.R;
-import eu.pinnoo.appsforghent2013.models.DataModel;
 
 public class GetWaitActivity extends Activity {
 	 /** Called when the activity is first created. */
@@ -11,6 +10,5 @@ public class GetWaitActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.messages_layout);
-        InfoPopup.showPopup(this, DataModel.getInstance().getApothecarys().get("kml_1"));
     }
 }
