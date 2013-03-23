1af3c574af0c035023d350d6de1195b9b44c4b2a
Wouter Pinnoo <Wouter.Pinnoo@UGent.be>
2013-03-23 14:58:28 +0100

    implemented mapview

--- src/eu/pinnoo/appsforghent2013/view/LocateActivity.java
+++ src/eu/pinnoo/appsforghent2013/view/LocateActivity.java
@@ -1,15 +1,28 @@
 package eu.pinnoo.appsforghent2013.view;
 
-import android.app.Activity;
 import android.os.Bundle;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
 import eu.pinnoo.appsforghent2013.R;
 
 
-public class LocateActivity extends Activity {
-	 /** Called when the activity is first created. */
+public class LocateActivity extends MapActivity {
+
+    private MapView mapView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.friends_layout);
+        mapView = (MapView) findViewById(R.id.mapView);
     }
+
+    public void onStatusChanged(String string, int i, Bundle bundle) {
+    }
+
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
+
 }
