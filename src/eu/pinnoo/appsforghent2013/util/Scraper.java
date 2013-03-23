98afa580bf73291768d4acee241f9fc62929ecf6
Wouter Pinnoo <Wouter.Pinnoo@UGent.be>
2013-03-23 11:38:32 +0100

    implemented scraper

---
+++ src/eu/pinnoo/appsforghent2013/util/Scraper.java
@@ -0,0 +1,92 @@
+package eu.pinnoo.appsforghent2013.util;
+
+import android.R.array;
+import eu.pinnoo.appsforghent2013.models.DataModel;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Wouter Pinnoo <Wouter.Pinnoo@UGent.be>
+ */
+public class Scraper {
+
+    public void loadData(DataModel model){
+        JSONArray arr = downloadData();
+        fetchData(arr, model);
+    }
+    
+    protected static InputStream getStream(String full_url) {
+        try {
+            URL url = new URL(full_url);
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setConnectTimeout(1000);
+            return urlConnection.getInputStream();
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+
+    protected static JSONArray downloadData() {
+        InputStream inp = getStream("http://datatank.gent.be/Gezondheid/Apotheken.json");
+        String result = "";
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inp, "iso-8859-1"), 8);
+            StringBuilder builder = new StringBuilder();
+            builder.append(reader.readLine()).append("\n");
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                builder.append(line).append("\n");
+            }
+            inp.close();
+            result = builder.toString();
+
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(Scraper.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        JSONArray arr = null;
+        try {
+            if (!result.isEmpty() && !result.equals("null\n")) {
+                JSONObject obj = new JSONObject(result);
+                arr = obj.getJSONArray("Apotheken");
+            }
+        } catch (JSONException ex) {
+            Logger.getLogger(Scraper.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            return arr;
+        }
+    }
+    
+    protected static void fetchData(JSONArray arr, DataModel model){
+        for (int i = 0; i < arr.length(); i++) {
+            JSONObject obj = null;
+            try {
+                obj = arr.getJSONObject(i);
+                Apothecary a = new Apothecary(Float.parseFloat(obj.getString("lat")), Float.parseFloat(obj.getString("long")), obj.getString("naam"), obj.getString("adres"), obj.getInt("distance"), obj.getString("id"), obj.getString("fid"), Integer.parseInt(obj.getString("postcode")), obj.getString("gemeente"));
+                model.addApothecary(a);
+                } catch (JSONException ex) {
+                Logger.getLogger(Scraper.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (NullPointerException e) {
+                System.err.println(e);
+            }
+        }
+        
+        
+    }
+}
--- src/eu/pinnoo/appsforghent2013/view/MainActivity.java
+++ src/eu/pinnoo/appsforghent2013/view/MainActivity.java
@@ -1,44 +1,10 @@
 package eu.pinnoo.appsforghent2013.view;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnShowListener;
-import android.content.Intent;
-import android.graphics.Color;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.InputFilter;
-import android.text.InputType;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Switch;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
-import eu.pinnoo.debtapp.Debt;
-import eu.pinnoo.debtapp.DecimalDigitsInputFilter;
-import eu.pinnoo.debtapp.R;
-import eu.pinnoo.debtapp.User;
-import eu.pinnoo.debtapp.database.DAO;
-import eu.pinnoo.debtapp.models.UserModel;
-import static eu.pinnoo.debtapp.models.UserModel.DIRECTION.EAST;
-import static eu.pinnoo.debtapp.models.UserModel.DIRECTION.WEST;
-import java.util.Iterator;
-import java.util.List;
+import eu.pinnoo.appsforghent2013.R;
+
+
 
 /**
  *
