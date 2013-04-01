4c9fe73a6789beb4c32825303f1624e1ca8ce901
Wouter Pinnoo <pinnoo.wouter@gmail.com>
2013-04-02 01:03:14 +0200

    fixed #15

--- src/mobi/pharmaapp/util/LocalConstants.java
+++ src/mobi/pharmaapp/util/LocalConstants.java
@@ -1,8 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package mobi.pharmaapp.util;
+
+import com.google.android.maps.GeoPoint;
 
 /**
  *
@@ -13,5 +11,6 @@ public class LocalConstants {
     public static final String ENCODING = "iso-8859-1";
     public static final String EMERGENCY_JSON = "http://data.pharmaapp.mobi/em_pharms.json";
     public static final String PHARMACY_JSON = "http://datatank.gent.be/Gezondheid/Apotheken.json";
+    public static final GeoPoint INITIAL_USER_LOCATION = new GeoPoint(51100607, 3763328);
 
 }
